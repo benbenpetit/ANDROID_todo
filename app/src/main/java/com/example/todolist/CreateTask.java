a447b9355c05c1da04993462579f1c8bbcf2e7c6
Benoit PETIT <petitbenoit3@gmail.com>
2022-02-02 17:44:24 +0100

    first commit

---
+++ app/src/main/java/com/example/todolist/CreateTask.java
@@ -0,0 +1,30 @@
+package com.example.todolist;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+
+public class CreateTask extends Fragment {
+
+    private ArrayList<String> tasks;
+    private ArrayAdapter<String> tasksAdapter;
+    private TextView title;
+    private TextView date;
+    private Button button;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.createtask_layout, container, false);
+    }
+}
